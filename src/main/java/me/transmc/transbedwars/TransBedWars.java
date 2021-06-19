77c929829332bffffb8a64b4ff8ef5653b0498ae
Michele Pipicelli <ava@avaisgay.me>
2021-06-19 10:54:53 +0930

    Initial Commit uwu

---
+++ src/main/java/me/transmc/transbedwars/TransBedWars.java
@@ -0,0 +1,17 @@
+package me.transmc.transbedwars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TransBedWars extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
